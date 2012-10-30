3289fb2ea7d6e1b2f1dd1c261b0362d78f68124d
William Brown <william@firstyear.id.au>
2012-10-30 19:42:08 +1030

    Added tests

--- blogalog/tests.py
+++ blogalog/tests.py
@@ -8,9 +8,27 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class BlogalogViewsTest(TestCase):
+    fixtures = ['blogalog-01-testdata.json']
+
+    def test_index(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test the index view, which lists our most recent, visible, posts.
         """
-        self.assertEqual(1 + 1, 2)
+        resp = self.client.get('/blog/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('latest_blog_entries' in resp.context)
+        self.assertEqual([entry.pk for entry in resp.context['latest_blog_entries']], [2, 1])
+        entry_1 = resp.context['latest_blog_entries'][1]
+        self.assertEqual(entry_1.title, 'Test entry ')
+
+    def test_detail(self):
+        """
+        Test the detail view
+        """ 
+        resp = self.client.get('/blog/entry/1')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.context['entry'].pk, 1)
+        #Ensure that non-existant polls make a 404 happen!
+        resp = self.client.get('/blog/entry/3')
+        self.assertEqual(resp.status_code, 404)
